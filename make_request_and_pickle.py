f8f7d895b9391921bfaddbec2ff608344e337396
JLAT <jlat0128@gmail.com>
2020-03-19 12:15:24 -0400

    Woekinf

--- make_request_and_pickle.py
+++ make_request_and_pickle.py
@@ -2,22 +2,21 @@ import urllib.request
 import json
 import pickle
 
-file_start = 0
 offset_counter = 1
 file_counter = 0
 
-while file_counter < 1:
+while file_counter < 39:
     url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limt=1000&' + 'offset=' + str(offset_counter)
     headers = {'token': 'XedyBNDLDGtaqOCjNDlpEBYOyGyRlRvc'}
     req = urllib.request.Request(url, headers=headers)
-    file_name = './location_' + str(file_start) + '.json'
+    file_name = './location_' + str(file_counter) + '.json'
     
     ##parsing response
     with urllib.request.urlopen(req) as f:
-        data = json.loads(f)
-        
+        data = json.load(f)
+
         with open(file_name, 'wb') as handler:
             pickle.dump(data, handler)
-        
-        file_counter += 1
-        offset_counter += 1000
+    
+    file_counter += 1
+    offset_counter += 1000
