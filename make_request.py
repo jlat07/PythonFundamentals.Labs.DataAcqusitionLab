446d14f12ae34e61a4b1ee003010f3664a556914
JLAT <jlat0128@gmail.com>
2020-03-19 11:54:13 -0400

    was not dumping json file. updated that

--- make_request.py
+++ make_request.py
@@ -7,14 +7,17 @@ offset_counter = 1
 file_start = 0
 
 while file_counter < 1:
-    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/' + 'offset=' + str(offset_counter)
+    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limt=1000&' + 'offset=' + str(offset_counter)
     headers = {'token': 'XedyBNDLDGtaqOCjNDlpEBYOyGyRlRvc'}
     req = urllib.request.Request(url, headers=headers)
-    file_name = './location_' + str(file_start) + '.json'
+    file_name = './location_'+str(file_start)+'.json'
     
     ##parsing response
     with urllib.request.urlopen(req) as f:
         data = json.loads(f)
-        
+    
+        with open(file_name, 'wb') as handler:
+            json.dump(data, handler)
+            
     file_counter += 1
     offset_counter += 1000
--- make_request_and_pickle.py
+++ make_request_and_pickle.py
@@ -7,7 +7,7 @@ offset_counter = 1
 file_counter = 0
 
 while file_counter < 1:
-    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/' + 'offset=' + str(offset_counter)
+    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limt=1000&' + 'offset=' + str(offset_counter)
     headers = {'token': 'XedyBNDLDGtaqOCjNDlpEBYOyGyRlRvc'}
     req = urllib.request.Request(url, headers=headers)
     file_name = './location_' + str(file_start) + '.json'
