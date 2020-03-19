6b0dd9482f3b4acc119dedb38ade836ca365677a
JLAT <jlat0128@gmail.com>
2020-03-19 11:05:00 -0400

    updating code, url headers

--- make_request.py
+++ make_request.py
@@ -1,4 +1,4 @@
-
+'''
 2. Using [urllib.request](https://docs.python.org/3/library/urllib.request.html#module-urllib.request) module, make it so that your script makes rest calls to NOAA's api and saves the results to json files.
 3. Once it is executed, you should have 39 files.
 The files should be named as follows:
@@ -11,19 +11,20 @@ make an api call
 store data as a variable in the code
 save the data as a file.
 file name has to iterate or +1 to incriment file name for every pull
-
+'''
 
 import urllib.request
 import json
 
 
 url = ' https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/'
-req = urllib.request.Request(url)
+headers = {'token':	'XedyBNDLDGtaqOCjNDlpEBYOyGyRlRvc'}
+req = urllib.request.Request(url, headers = headers)
 
 ##parsing response
-r = urllib.request.urlopen(req).read()
-cont = json.loads(r.decode('utf-8'))
-counter = 0
+with urllib.request.urlopen(req) as f:
+    data = json.loads(f)
+    counter = 0
 
 ##parcing json
 for item in cont['data']['children']:
@@ -35,7 +36,9 @@ print("----")
 # print (json.dumps(cont, indent=4, sort_keys=True))
 print("Number of titles: ", counter)
 
+''''
 what i have about looks to pull the request. read yhte json file, store it in cont then
 oarce through it and print it. I would take each parche. pickle it out?
 
 use the counter to update a location_'x'.json file name and impliment it through interation
+'''
