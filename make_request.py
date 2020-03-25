3fe1e85612c5639c018c42f711b234bbbefd3e24
JLAT <jlat0128@gmail.com>
2020-03-25 14:30:07 -0400

    Re implemented environmental variables

--- make_request.py
+++ make_request.py
@@ -8,7 +8,7 @@ file_counter = 0
 
 while file_counter < 39:
     url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limit=1000&' + 'offset=' + str(offset_counter)
-    headers = {os.environ.get('token')}
+    headers = {'TOKEN':os.environ.get('TOKEN')}
     req = urllib.request.Request(url, headers=headers)
     file_name = './location_'+str(file_counter)+'.json'
     
@@ -20,3 +20,15 @@ while file_counter < 39:
             
     file_counter += 1
     offset_counter += 1000
+
+
+"""
+    # make the request ot noaa
+    call
+    noaa
+    # writes the results
+    write
+    results
+    # let the scrip sleep for 5 seconds for a bugger
+    wait
+"""
